042b3bb88a999a9c94559628bb27e3c5c829e2a5
bilaalukis <bilaalukis123@gmail.com>
2021-03-23 14:34:34 +0900

    Setup firebase auth with react frontend

---
+++ src/components/Frontpage.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Navigationbar from "./Navigationbar";
+
+export default function Frontpage() {
+  return <Navigationbar />;
+}
---
+++ src/components/UpdateProfile.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function UpdateProfile() {
+  return <div>sfsddsree</div>;
+}
